c55a7de42d1692f037f9cea2bf6033aa5fd95218
RichardT1996 <155005947+RichardT1996@users.noreply.github.com>
2025-11-17 14:30:50 +0000

    Update content in index.php
    Replaced placeholder text with a personal message.

--- site/index.php
+++ site/index.php
@@ -13,12 +13,13 @@
 	<h2>Welcome to my website!</h2>
 	<p>Some content goes here!</p>
 
+	<h3>
+		Hello my name is Richard Thomas.
+	</h3>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		This is for my DevOps module.
 	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+	
 </div>
 
 <?php include("includes/footer.php");
